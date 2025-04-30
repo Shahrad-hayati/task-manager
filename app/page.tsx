11c695cbfbc69e6f6d449f3fbcf9f0ed79f177ef
Shahrad Hayati <shahradhayati@yahoo.com>
2025-04-30 19:44:27 +0330

    homepage finished

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,46 @@ import {
   RegisterLink,
   LoginLink,
 } from '@kinde-oss/kinde-auth-nextjs/components'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const { isAuthenticated } = getKindeServerSession()
+  const isLoggedIn = await isAuthenticated()
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center'>
-      <h1 className='text-5xl font-bold text-center'>Welcome to DailySH</h1>
+      <div className='max-w-7xl mx-auto'>
+        <div className='text-center'>
+          <h1 className='text-4xl md:text-5xl lg:text-6xl font-bold tracking-tight'>
+            <p>Your personal workspace</p>
+            <p className='text-5xl md:text-6xl'>
+              for <span className='text-blue-600'>better productivity</span>
+            </p>
+          </h1>
+          <p className='mt-6 text-base lg:text-lg text-muted-foreground max-w-2xl mx-auto'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+            animi temporibus tempore?
+          </p>
+          <div className='flex items-center justify-center gap-4 mt-6'>
+            {isLoggedIn ? (
+              <>
+                <Button asChild>
+                  <Link href='/workspace'>Go to workspace</Link>
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button>
+                  <RegisterLink>Get Started</RegisterLink>
+                </Button>
 
-      <div className='flex mt-6 gap-4'>
-        <Button>
-          <RegisterLink>Get Started</RegisterLink>
-        </Button>
-
-        <Button asChild variant={'outline'}>
-          <LoginLink>Sign In</LoginLink>
-        </Button>
+                <Button asChild variant={'outline'}>
+                  <LoginLink>Sign in</LoginLink>
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   )
